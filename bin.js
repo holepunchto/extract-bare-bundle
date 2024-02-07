781fc28837d569de6257d6c4abc13e6f9fcb8cac
Mathias Buus <mathiasbuus@gmail.com>
2024-02-08 00:50:21 +0100

    add prebuild symlinks

--- bin.js
+++ bin.js
@@ -3,6 +3,8 @@ const Bundle = require('bare-bundle')
 const fs = require('fs')
 const path = require('path')
 
+const isWindows = process.platform === 'win32'
+const host = process.platform + '-' + process.arch
 const filename = process.argv[2]
 if (!filename) throw new Error('Usage: extract-bare-bundle filename')
 
@@ -14,4 +16,19 @@ for (const key of b._files.keys()) {
 
   fs.mkdirSync(dirname, { recursive: true })
   fs.writeFileSync(filename, b.read(key))
+}
+
+if (!isWindows) {
+  for (const [key, map] of Object.entries(b.resolutions)) {
+    const addon = map['bare:addon']
+    if (!addon) continue
+    const dirname = path.join('.', key, 'prebuilds', host)
+    const addonPath = path.resolve(path.join(filename, addon))
+    const symlinkPath = path.join(dirname, addon.split('/').pop().replace(/@[^.]+/g, ''))
+    fs.mkdirSync(dirname, { recursive: true })
+    try {
+      fs.unlinkSync(symlinkPath)
+    } catch {}
+    fs.symlinkSync(addonPath, symlinkPath)
+  }
 }
